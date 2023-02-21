e138a9fae4b9837eb233a2f83115b31158ead605
Marcus-A-B <mburns05@student.bbk.ac.uk>
2023-02-21 02:30:13 +0000

    1st question done

--- src/reflection/DescribeClass.java
+++ src/reflection/DescribeClass.java
@@ -1,10 +1,24 @@
 package reflection;
 
+import java.util.Arrays;
+
 public class DescribeClass {
     public static void main(String[] args) {
         if (args.length != 1)
-            System.out.println("Usage: "); // TODO: describe how to use the utility
+            System.out.println("Usage: Supply exactly one parameter which is the name of the class you want to inspect"); // TODO: describe how to use the utility
 
         // TODO: implement the functionality for Question 1
+        Class<?> c = null;
+        try
+        {
+            c = (Class<?>) Class.forName(args[0]);
+        }
+        catch (ClassNotFoundException e)
+        {
+            throw new RuntimeException(e);
+        }
+
+        Class<?>[] interfaces= c.getInterfaces();
+        Arrays.stream(interfaces).forEach(s -> System.out.println(s.toString()));
     }
 }
