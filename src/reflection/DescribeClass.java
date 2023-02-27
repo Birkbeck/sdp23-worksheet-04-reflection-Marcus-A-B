be7d08934262549d60a4dab4256ab56fdc06fc18
Marcus-A-B <mburns05@student.bbk.ac.uk>
2023-02-27 02:13:16 +0000

    2nd question in progress

--- src/reflection/DescribeClass.java
+++ src/reflection/DescribeClass.java
@@ -1,17 +1,21 @@
 package reflection;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 import java.util.Arrays;
 
 public class DescribeClass {
     public static void main(String[] args) {
-        if (args.length != 1)
+        if (args.length != 1) {
             System.out.println("Usage: Supply exactly one parameter which is the name of the class you want to inspect"); // TODO: describe how to use the utility
-
+            System.exit(0);
+        }
         // TODO: implement the functionality for Question 1
-        Class<?> c = null;
+        Class<?> c;
         try
         {
-            c = (Class<?>) Class.forName(args[0]);
+            c = Class.forName(args[0]);
         }
         catch (ClassNotFoundException e)
         {
@@ -20,5 +24,38 @@ public class DescribeClass {
 
         Class<?>[] interfaces= c.getInterfaces();
         Arrays.stream(interfaces).forEach(s -> System.out.println(s.toString()));
+
+        System.out.println(Arrays.toString(c.getInterfaces()));
+        System.out.println(c.getName());
+        System.out.println(Arrays.toString(c.getClasses()));
+        System.out.println(c.arrayType());
+        System.out.println(c);
+        System.out.println(Arrays.toString(c.getDeclaredFields()));
+
+        Method[] m = c.getMethods();
+        Field[] f = c.getFields();
+
+        Constructor con = null;
+        try {
+            con = c.getConstructor();
+        }catch(NoSuchMethodException e)
+        {
+            System.out.println(e);
+            System.out.println();
+        }
+
+        System.out.println("Methods:");
+        for (Method item:m) System.out.println(item);
+        System.out.println();
+        System.out.println("Fields:");
+        for (Field item:f) System.out.println(item);
+        System.out.println();
+        System.out.println("Constructor:");
+        System.out.println(con);
+        System.out.println();
+        System.out.println("End");
+
+
+
     }
 }
--- src/reflection/InstantiateClass.java
+++ src/reflection/InstantiateClass.java
@@ -1,10 +1,41 @@
 package reflection;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
 public class InstantiateClass {
     public static void main(String[] args) {
         if (args.length < 1)
-            System.out.println("Usage: "); // TODO: describe how to use the utility
+        {
+            System.out.println("Usage: Supply the class name as the first parameter followed by each constructor argument thereafter"); // TODO: describe how to use the utility
+            System.exit(0);
+        }
 
         // TODO: implement the functionality for Question 2
+        Class<?> cl = null;
+        try
+        {
+            cl = Class.forName(args[0]);
+        }
+        catch (ClassNotFoundException e)
+        {
+            System.out.println(e);
+        }
+
+        Class<?>[] argTypes = new Class[args.length - 1];
+        for(int i = 1; i < args.length; i++)
+        {
+            argTypes[i] = args[i].getClass();
+        }
+
+        Constructor con = null;
+        try {
+            con = cl.getConstructor(argTypes);
+        }
+        catch (NoSuchMethodException e)
+        {
+            System.out.println(e);
+        }
     }
 }
